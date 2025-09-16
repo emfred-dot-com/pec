22e202f93128836d2d86b9fc6e7c5394e8a03ad0
Eric Frederickson <ericfrederickson68@gmail.com>
2025-09-15 21:43:38 -0500

    improve the speed and convenience of completion

--- completion-setup.el
+++ completion-setup.el
@@ -2,22 +2,18 @@
 ;;; completion-setup.el -- Configurations for (auto) completion
 ;;;
 
-;; Hotfuzz (new): better version of the builtin `flex' completion
-;; style
-(use-package hotfuzz)
-
 ;; Orderless: brilliant completion style that I use everywhere
 (use-package orderless
-  :config (setq completion-styles '(orderless hotfuzz basic)))
+  :config
+  (setq completion-styles
+	'(orderless basic)
+	orderless-matching-styles
+	'(orderless-literal orderless-regexp)))
 
-(setq
- completion-ignore-case t
- read-buffer-completion-ignore-case t
- read-file-name-completion-ignore-case t
- bookmark-completion-ignore-case t
- completion-category-defaults nil
- completion-category-overrides
- '((file (styles . (partial-completion hotfuzz)))))
+(setq completion-ignore-case t
+      read-buffer-completion-ignore-case t
+      read-file-name-completion-ignore-case t
+      bookmark-completion-ignore-case t)
 
 ;; Corfu: frontend for auto completion with various backends
 (use-package corfu
@@ -25,23 +21,21 @@
   (corfu-auto t)
   (corfu-auto-delay 0.1)
   (corfu-auto-prefix 2)
-  (corfu-cycle t)              ;; Enable cycling for `corfu-next/previous'
+  (corfu-cycle t)
   (corfu-echo-delay 0.25)
-  (corfu-separator ?\s)        ;; Orderless field separator
-  (corfu-quit-at-boundary nil) ;; Never quit at completion boundary
-  (corfu-quit-no-match t)      ;; Never quit, even if there is no match
-  (corfu-preview-current nil)  ;; Disable current candidate preview
-  (corfu-preselect 'first)     ;; Preselect the prompt
-  (corfu-on-exact-match nil)   ;; Configure handling of exact matches
-  (corfu-scroll-margin 5)      ;; Use scroll margin
+  (corfu-separator ?\s)
+  (corfu-quit-at-boundary 'separator)
+  (corfu-quit-no-match t)
+  (corfu-preview-current 'insert)
+  (corfu-preselect 'valid)
+  (corfu-on-exact-match 'insert)
+  (corfu-scroll-margin 5)
   :init
   (corfu-echo-mode)
   (corfu-history-mode)
   (global-corfu-mode))
 
-(setq
- completion-cycle-threshold 1
- tab-always-indent 'complete)
+(setq tab-always-indent 'complete)
 
 ;; Pabbrev: a great simple completion solution.
 ;; Using Corfu at the moment but keeping
