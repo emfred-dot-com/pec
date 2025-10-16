4da15fde615445ef084d47bb55c91270fed95d17
Eric Frederickson <ericfrederickson68@gmail.com>
2025-10-16 16:20:32 -0400

    org-setup: Add some refinements for templates and electric-pairs
    - Load the 'org-tempo' library, enabling you to, for example, add a
      "#+begin_source" block by typing the abbreviation "<s" and then
      typing "TAB".
    
    - Following that, disable the auto-insertion of ">" after "<" in
      org-mode, to not interfere with the template insertion mentioned
      above.

--- org-setup.el
+++ org-setup.el
@@ -2,14 +2,40 @@
 ;;; org-setup.el -- Configurations for org-mode
 ;;;
 
-(use-package org-bullets
-  :hook (org-mode . org-bullets-mode))
+(use-package org
+  :config
 
-(add-hook 'org-mode-hook
-	  (=> (flyspell-mode 0)
-	      (org-indent-mode 1)
-	      (abbrev-mode 1)
-	      (setq captain-predicate
-		    (=> (not (org-in-src-block-p)))
-		    org-image-max-width 50)
-	      (keybind-local "C-c h" consult-org-heading)))
+  ;; Load the `org-mode' templates for the built-in `tempo' templating
+  ;; package. This provides a menu of snippets of the form "<X" where
+  ;; "X" is some letter, and all of these snippets expand to org block
+  ;; directives. For example, typing "<s" followed by typing "TAB"
+  ;; will expand to the following (where "." is where it leaves the
+  ;; cursor):
+  ;;
+  ;; "#+begin_source .
+  ;;
+  ;; #+end_source"
+  ;;
+  ;; Other out-of-the-box snippets are "<q" for a quote block, "<v"
+  ;; for a verse block, etc.
+  (require 'org-tempo)
+
+  (add-hook 'org-mode-hook
+	    (=> (flyspell-mode 0)
+		(org-indent-mode 1)
+		(abbrev-mode 1)
+
+		(setq-local
+		 captain-predicate
+		 (=> (not (org-in-src-block-p)))
+		 org-image-max-width 50
+		 completion-at-point-functions
+		 '(pcomplete-completions-at-point cape-dabbrev)
+		 electric-pair-inhibit-predicate
+		 (lambda (char) (eq char ?<)))
+
+		(keybind-local "C-c h" consult-org-heading))))
+
+(use-package org-bullets
+  :after org
+  :hook (org-mode . org-bullets-mode))
