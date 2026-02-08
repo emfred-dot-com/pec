4eb11e6ccbba5540fd77c2994d1ce6959e1bf6dd
Jake Freeland <jfree@FreeBSD.org>
2026-02-08 16:33:34 -0600

    lsp-setup: Use eglot instead of lsp-mode
    Eglot has been built into Emacs since version 29.1. It is more minimal
    than lsp-mode, priorizing a good out-of-box experience. It hooks into
    completion-in-region, so it can be used with corfu and orderless capf.

--- lsp-setup.el
+++ lsp-setup.el
@@ -2,10 +2,12 @@
 ;;; lsp-setup.el -- Setup for Language Server Protocol integration
 ;;;
 
-(use-package lsp-mode
-  :defer t)
+(use-package eglot
+  :after orderless
+  :init
+  (setq eglot-autoshutdown t
+        eglot-code-action-indications '(margin)
+        completion-category-overrides '((eglot (styles orderless))
+                                        (eglot-capf (styles orderless)))))
 
-(add-hook 'lsp-mode-hook #'lsp-enable-which-key-integration)
 
-(add-hook 'lsp-mode-hook
-	  (=> (keybind-local "M-." lsp-find-definition)))
