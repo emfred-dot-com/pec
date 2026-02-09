2d1b086151e73c51aa7b7fa1934d5d1411cdf057
Jake Freeland <jfree@FreeBSD.org>
2026-02-09 10:24:24 -0600

    windows-setup: Split and focus
    Automatically focus the new window on vertical and horizontal split.

--- windows-setup.el
+++ windows-setup.el
@@ -2,10 +2,22 @@
 ;;; windows-setup.el -- Window management
 ;;;
 
+(defun split-window-below-and-focus ()
+  (interactive)
+  (split-window-below)
+  (other-window 1))
+
+(defun split-window-right-and-focus ()
+  (interactive)
+  (split-window-right)
+  (other-window 1))
+
 (use-package emacs
   :config
   ;; Easy window switch
   (keybinds "C-<tab>" other-window
+            "C-x 2" split-window-below-and-focus
+            "C-x 3" split-window-right-and-focus
             "C-S-i" enlarge-window
             "C-S-d" shrink-window))
 
