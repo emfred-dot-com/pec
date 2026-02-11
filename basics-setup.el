be52a653f54b7cec29cdb590136038f244715203
Eric Frederickson <ericfrederickson68@gmail.com>
2026-02-11 13:11:39 -0500

    basics-setup: Change 'toggle-frame-fullscreen' binding from "C-z f" to "C-z F"
    This avoids a conflict with the recently added "C-z f" binding for
    'consult-find' in consult-setup.

--- basics-setup.el
+++ basics-setup.el
@@ -6,7 +6,7 @@
   :config
   (keybinds
    "C-x c" restart-emacs
-   "C-z f" toggle-frame-fullscreen))
+   "C-z F" toggle-frame-fullscreen))
 
 ;; Better yes-or-no prompt (e.g. upon "C-c C-c")
 (use-package ido-yes-or-no
